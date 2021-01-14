119876ac2dfa0ee5330a23869eaf9806b77c70f9
oralbrown <brown.oral2015@gmail.com>
2021-01-14 14:44:21 -0500

    this show all changes to js and css
    co-authored by Ashley Porter(porter.ashley13@gmail.com)

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  const age = parseInt(prompt("How old are you?"));
+  if (age >=18) {
+    $('#voting-age').show();
+  } else {
+    $("#under-18").show();
+  }
+});
